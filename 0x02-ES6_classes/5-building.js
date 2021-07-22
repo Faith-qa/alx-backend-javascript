48d9ad10c09e4948ff63d8c63ea5ce4fe136c71c
Faith-qa <atienofaith12@gmail.com>
2021-07-22 07:45:58 +0300

    corrected parsing error

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,12 @@
 class Building {
   constructor(sqft) {
-    if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw Error('class extending building must override evacuationWarningMessage');
-    }
+    if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('class extending building must override evacuationWarningMessage');
+
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(value) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-    this._sqft = value;
   }
 }
 export default Building;
