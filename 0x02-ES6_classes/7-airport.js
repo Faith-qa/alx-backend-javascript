bbffa8227b497e51964c9ef713a4101d68c223a1
Faith-qa <atienofaith12@gmail.com>
2021-07-24 18:39:11 +0300

    fixed errors

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,14 +1,10 @@
-class Airport {
+export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
   }
 
-  // eslint-disable-next-line class-methods-use-this
   toString() {
-    // eslint-disable-next-line no-template-curly-in-string
-    return '[${typeof this} ${this._code}]';
+    return `[${typeof this} ${this._code}]`;
   }
 }
-
-export default Airport;
